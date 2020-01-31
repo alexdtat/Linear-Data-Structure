05c70504a85ae24f9fd9d947cdd142c85f64b392
alexdtat <57017816+alexdtat@users.noreply.github.com>
2020-02-01 02:27:09 +0300

    Update main.cpp
    Array

--- main.cpp
+++ main.cpp
@@ -2,66 +2,45 @@
 
 using namespace std;
 
-struct Simple_vector{
-    int vec_size;
-    int vec_capacity;
+struct Simple_array{
+    int arr_size;
     int* data;
 
-    Simple_vector (int x){
-        vec_size = x;
-        vec_capacity = x;
-        data = new int[vec_capacity];
+    Simple_array (int x){
+        arr_size = x;
+        data = new int[arr_size];
     }
 
-    ~Simple_vector(){
+    ~Simple_array(){
         delete [] data;
     }
 
 
-    Simple_vector (const Simple_vector &V){
-        if (this != &V){
-            vec_size = V.vec_size;
-            vec_capacity = V.vec_capacity;
-            data = new int[vec_capacity];
-            for (int i = 0; i < vec_size; i++){
-                data[i] = V.data[i];
+    Simple_array (const Simple_array &A){
+        if (this != &A){
+            arr_size = A.arr_size;
+            data = new int[arr_size];
+            for (int i = 0; i < arr_size; i++){
+                data[i] = A.data[i];
             }
         }
     }
 
-    const Simple_vector& operator= (const Simple_vector& V){
-        if (this == &V) return *this;
+    const Simple_array& operator= (const Simple_array& A){
+        if (this == &A) return *this;
         delete [] data;
-        vec_size = V.vec_size;
-        vec_capacity = V.vec_capacity;
-        data = new int[vec_capacity];
-        for (int i = 0; i < vec_size; i++) data[i] = V.data[i];
+        arr_size = A.arr_size;
+        data = new int[arr_size];
+        for (int i = 0; i < arr_size; i++) data[i] = A.data[i];
         return *this;
     }
 
     int &operator [] (int i){
-        if ((i < 0) || (i >= vec_size)){
+        if ((i < 0) || (i >= arr_size)){
             cout << "Err\n";
             return data[0];
         };
         return data[i];
-    }
-
-    void push_back_vec (int x){
-        if (vec_size == vec_capacity){
-            vec_capacity *= 2;
-            int* temp = new int [vec_capacity];
-            for (int i = 0; i < vec_size; i++) temp[i] = data[i];
-            delete [] data;
-            data = temp;
-        }
-        data [vec_size] = x;
-        vec_size++;
-    }
-
-    void resize_vec (int s){
-        if (s > vec_capacity) vec_capacity += s - vec_capacity;
-        vec_size = s;
     }
 };
 
@@ -69,24 +48,14 @@ int main()
 {
     int test_size = 0;
     int usnum = 0;
-    int usel = 0;
-    int ussize = 0;
-    cout << "Vector size input:\n";
+    cout << "Array size input:\n";
     cin >> test_size;
-    Simple_vector test_vec(test_size);
-    cout << "The test vector is created.\n";
-    Simple_vector second_vec = test_vec;
-    cout << "The identical vector is created.\n";
+    Simple_array test_arr(test_size);
+    cout << "The test array is created.\n";
+    Simple_array second_arr = test_arr;
+    cout << "The identical array is created.\n";
     cout << "Input a number:\n";
     cin >> usnum;
-    cout << "The " << usnum << " element of the vector: " << test_vec [usnum] << '\n';
-    cout << "Input a number for new element creation:\n";
-    cin >> usel;
-    test_vec.push_back_vec (usel);
-    cout << "The vector has been updated.\n";
-    cout << "Input the new size of vector:\n";
-    cin >> ussize;
-    test_vec.resize_vec(ussize);
-    cout << "The size has been updated.\n";
+    cout << "The " << usnum << " element of the array: " << test_arr [usnum] << '\n';
     return 0;
 }
